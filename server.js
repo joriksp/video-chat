4b9a15cf81929a591fdb6d7a0dfc4e88017672b4
pilotikFN <79021423+pilotikFN@users.noreply.github.com>
2024-08-10 14:10:51 +0400

    more fixes

--- server.js
+++ server.js
@@ -4,36 +4,44 @@ const server = require("http").Server(app);
 const { v4: uuidv4 } = require("uuid");
 app.set("view engine", "ejs");
 const io = require("socket.io")(server, {
-  cors: {
-    origin: '*'
-  }
+   cors: {
+      origin: "*",
+   },
 });
 const { ExpressPeerServer } = require("peer");
 const opinions = {
-  debug: true,
-}
+   debug: true,
+};
 
 app.use("/peerjs", ExpressPeerServer(server, opinions));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.redirect(`/${uuidv4()}`);
+   res.redirect(`/${uuidv4()}`);
 });
 
 app.get("/:room", (req, res) => {
-  res.render("room", { roomId: req.params.room });
+   res.render("room", { roomId: req.params.room });
 });
 
 io.on("connection", (socket) => {
-  socket.on("join-room", (roomId, userId, userName) => {
-    socket.join(roomId);
-    setTimeout(()=>{
-      socket.to(roomId).broadcast.emit("user-connected", userId);
-    }, 1000)
-    socket.on("message", (message) => {
-      io.to(roomId).emit("createMessage", message, userName);
-    });
-  });
+   socket.on("join-room", (roomId, userId, userName) => {
+      socket.join(roomId);
+      setTimeout(() => {
+         socket.to(roomId).broadcast.emit("user-connected", userId);
+      }, 1000);
+      socket.on("message", (message) => {
+         io.to(roomId).emit("createMessage", message, userName);
+      });
+   });
 });
 
-server.listen(process.env.PORT || 3030);
+// Export as a Vercel function
+module.exports = (req, res) => {
+   return new Promise((resolve) => {
+      server.listen(3000, () => {
+         console.log("Server is running on port 3000");
+         resolve();
+      });
+   });
+};
